11aa279dae79719b3fb151f0206a156339f47470
psalmex <samexbaba4all@gmail.com>
2021-03-08 12:03:56 +0100

    stadia in progress

---
+++ react-work/src/component/Stadia/games.js
@@ -0,0 +1,64 @@
+const games = [
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/assassinscreed_odyssey_1x.jpg" alt="" />,
+      name: "assassinscreed_odysse",
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/ary_secretofseasons.jpg" alt="" />,
+      name: "engineer",
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/ary_secretofseasons.jpg" alt="" />,
+      name: "Stylist",
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+    {
+      image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+      name: "cartoon"
+    },
+  ];
+  
+  export default games;
---
+++ react-work/src/data.js
@@ -0,0 +1,64 @@
+const students = [
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/assassinscreed_origins_1x.jpg" alt="" />,
+    name: "Assassinscreed_origins"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/ary_secretofseasons.jpg" alt="" />,
+    name: "Engineer",
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/assassinscreed_syndicate_1x.jpg" alt="" />,
+    name: "Assassinscreed_syndicate",
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/assassinscreed_valhalla_1x.jpg" alt="" />,
+    name: "Assassinscreed_valhalla"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Destiny_2__The_Collection_1x.jpg" alt="" />,
+    name: "Destiny_2"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/everspace_1x.jpg" alt="" />,
+    name: "Everspace"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/figment_1x.jpg" alt="" />,
+    name: "Figment"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Gunsport_1x.jpg" alt="" />,
+    name: "Gunsport"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/journey-savage_1x.jpg" alt="" />,
+    name: "journey-savage"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/monsterjam_steeltitans_1x.jpg" alt="" />,
+    name: "Monsterjam_steeltitans"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/ary_secretofseasons.jpg" alt="" />,
+    name: "Ary_secretofseasons"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Crayta_1x.jpg" alt="" />,
+    name: "Crayta"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/elhijo_1x.jpg" alt="" />,
+    name: "Elhijo"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/entertheguneon_1x.jpg" alt="" />,
+    name: "Entertheguneon"
+  },
+  {
+    image: <img src="https://www.gstatic.com/stadia/gamers/landing_page/assets/games/Borderlands3_1x.jpg" alt="" />,
+    name: "Borderlands3"
+  },
+];
+
+export default students;
