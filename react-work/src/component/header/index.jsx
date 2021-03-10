6c047eeded5f8f8983e5b36256584cd62f8593b2
psalmex <samexbaba4all@gmail.com>
2021-03-10 22:15:08 +0100

    header

--- react-work/src/component/header/index.jsx
+++ react-work/src/component/header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div style={{ backgroundColor: 'wine', textAlign: 'center' }}>
             
-            <h4>hopping_bag_white Play Cyberpunk 2077 on Stadia!</h4>
+            <h4><i class="fa fa-shopping-bag" aria-hidden="true"></i>bag_white Play Cyberpunk 2077 on Stadia!</h4>
             <a>Click here to Buy Now.</a> No subscription required.
         </div>
     );
